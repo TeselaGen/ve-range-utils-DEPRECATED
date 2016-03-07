a591044fad62733e5131dc20b155c107a2fdcd4a
Thomas Rich <trich1230@gmail.com>
2016-03-07 11:50:28 -0800

    reverting es6 syntax to es5

--- getRangeAngles.js
+++ getRangeAngles.js
@@ -4,9 +4,9 @@ module.exports = function getRangeAngles(range, rangeMax) {
     var startAngle = 2 * Math.PI * (range.start / rangeMax)
     var totalAngle = rangeLength / rangeMax * Math.PI * 2
     return {
-                startAngle,
+                startAngle: startAngle,
                 endAngle: 2 * Math.PI * (range.end + 1) / rangeMax, //use a +1 here because the angle must encompass the end of the annotation
-                totalAngle,
+                totalAngle: totalAngle,
                 centerAngle: startAngle + totalAngle/2 
             }
 };
