2b8303670664f6ad73ec4752962a7425936f5a50
Thomas Rich <trich1230@gmail.com>
2016-08-19 14:11:10 -0700

    adding ability to specify inclusive1BasedStart/End for the invert range function as a final param

--- invertRange.js
+++ invertRange.js
@@ -1,9 +1,37 @@
 var normalizePositionByRangeLength = require('./normalizePositionByRangeLength');
-module.exports = function invertRange(range, rangeMax) {
-    var start = range.end + 1;
-    var end = range.start - 1;
-    return {
+// module.exports = wrapInvertRange(invertRange)
+module.exports = function invertRange(range, rangeMax, options) {
+    options = options || {}
+    var newRange = {
+        start: range.start,
+        end: range.end
+    }
+    if (options.inclusive1BasedEnd) newRange.end--
+    if (options.inclusive1BasedStart) newRange.start--
+    
+    //invert the range! (everything is 0 based here )
+    var start = newRange.end + 1;
+    var end = newRange.start - 1;
+    
+    
+    var rangeToReturn = {
         start: normalizePositionByRangeLength(start, rangeMax, false),
         end: normalizePositionByRangeLength(end, rangeMax, false),
     }
+    
+    if (options.inclusive1BasedEnd) rangeToReturn.end++
+    if (options.inclusive1BasedStart) rangeToReturn.start++
+    return rangeToReturn
 }
+
+
+// function rangeOptions() {
+//     
+//     var newRange = {
+//         start: range.start,
+//         end: range.end
+//     }
+//     if (options.inclusive1BasedEnd) newRange.end--
+//     if (options.inclusive1BasedStart) newRange.start--
+//     
+// }
--- isRangeWithinRange.js
+++ isRangeWithinRange.js
@@ -1,3 +1,4 @@
+//TNR: THIS METHOD ONLY WORKS FOR COMPARING 0-BASED RANGES!!!!!!
 var trimRangeByAnotherRange = require('./trimRangeByAnotherRange')
 module.exports = function isRangeWithinRange(range1, range2, maxLength) {
     var ranges = trimRangeByAnotherRange(range1, range2, maxLength)
