69ac58971eb320e7727d9b0b1b581af5390a6ba5
Thomas Rich <trich1230@gmail.com>
2016-08-31 09:44:40 -0700

    adding modulateRangeBySequenceLength

---
+++ modulateRangeBySequenceLength.js
@@ -0,0 +1,11 @@
+var normalizePositionByRangeLength = require('./normalizePositionByRangeLength')
+var provideInclusiveOptions = require('./provideInclusiveOptions')
+var assign = require('lodash/object/assign')
+module.exports = provideInclusiveOptions(modulateRangeBySequenceLength)
+
+function modulateRangeBySequenceLength(range, seqLen) {
+    return assign(range, {
+        start: normalizePositionByRangeLength(range.start, seqLen),
+        end: normalizePositionByRangeLength(range.end, seqLen)
+    })
+}
