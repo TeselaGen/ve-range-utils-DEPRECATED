c881df40fda8cef9bd2bb698d26057a3f4b1ae13
Thomas Rich <trich1230@gmail.com>
2016-08-31 11:38:57 -0700

    fixing convertRangeIndices

--- convertRangeIndices.test.js
+++ convertRangeIndices.test.js
@@ -1,17 +1,24 @@
 var convertRangeIndices = require('./convertRangeIndices')
-var assert = require('assert');
+var chai = require('chai')
+chai.should();
 describe('convertRangeIndices', function () {
 	it('should correctly convert various types of ranges', function (done) {
-		assert(convertRangeIndices({start: 9,end:0},{inclusive1BasedStart: true}), {start: 8,end: 0})
-		assert(convertRangeIndices({start: 9,end:0},{inclusive1BasedStart: true}, {inclusive1BasedEnd: true}), {start: 8,end: 1})
-		assert(convertRangeIndices({start: 9,end:0},{inclusive1BasedEnd: true}, {inclusive1BasedEnd: true}), {start: 9,end: 0})
-		assert(convertRangeIndices({start: 4,end:5},{inclusive1BasedEnd: true}, {inclusive1BasedStart: true}), {start: 5,end: 4})
-		
-        
+		convertRangeIndices({start: 9,end:0},{inclusive1BasedStart: true}).should.deep.equal({start: 8,end: 0})
+		convertRangeIndices({start: 9,end:0},{inclusive1BasedStart: true}, {inclusive1BasedEnd: true}).should.deep.equal({start: 8,end: 1})
+		convertRangeIndices({start: 9,end:0},{inclusive1BasedEnd: true}, {inclusive1BasedEnd: true}).should.deep.equal({start: 9,end: 0})
+		convertRangeIndices({start: 4,end:5},{inclusive1BasedEnd: true}, {inclusive1BasedStart: true}).should.deep.equal({start: 5,end: 4})
+		convertRangeIndices({ 
+                start: '1',
+                end: '28',
+            }, 
+            {inclusive1BasedStart: true, inclusive1BasedEnd: true}, 
+            {})
+            .should.deep.equal({start: 0,end: 27})
 		done()
 	});
     it('should not remove other attributes on the range object', function (done) {
-    	assert(convertRangeIndices({start: 4,end:5, someOtherAttribute: 'yay'},{inclusive1BasedEnd: true}, {inclusive1BasedStart: true}), {start: 5,end: 4, someOtherAttribute: 'yay'})
+    	convertRangeIndices({start: 4,end:5, someOtherAttribute: 'yay'},{inclusive1BasedEnd: true}, {inclusive1BasedStart: true})
+            .should.deep.equal({start: 5,end: 4, someOtherAttribute: 'yay'})
 		done()
 	});
 })
