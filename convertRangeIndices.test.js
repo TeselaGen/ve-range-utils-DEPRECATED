8fc1c42cb08ea26b9f235dc4412a99f0356fd35d
Thomas Rich <trich1230@gmail.com>
2016-08-31 11:16:38 -0700

    adding another convertRangeIndices test

--- convertRangeIndices.test.js
+++ convertRangeIndices.test.js
@@ -6,6 +6,12 @@ describe('convertRangeIndices', function () {
 		assert(convertRangeIndices({start: 9,end:0},{inclusive1BasedStart: true}, {inclusive1BasedEnd: true}), {start: 8,end: 1})
 		assert(convertRangeIndices({start: 9,end:0},{inclusive1BasedEnd: true}, {inclusive1BasedEnd: true}), {start: 9,end: 0})
 		assert(convertRangeIndices({start: 4,end:5},{inclusive1BasedEnd: true}, {inclusive1BasedStart: true}), {start: 5,end: 4})
+		
+        
+		done()
+	});
+    it('should not remove other attributes on the range object', function (done) {
+    	assert(convertRangeIndices({start: 4,end:5, someOtherAttribute: 'yay'},{inclusive1BasedEnd: true}, {inclusive1BasedStart: true}), {start: 5,end: 4, someOtherAttribute: 'yay'})
 		done()
 	});
 })
