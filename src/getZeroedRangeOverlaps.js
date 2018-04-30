e29237eebf0b5d2b2d3a354470e8fdf84099fc9c
Thomas Rich <tnrich@teselagen.com>
2018-04-30 12:27:21 -0700

    adding a comment

--- src/getZeroedRangeOverlaps.js
+++ src/getZeroedRangeOverlaps.js
@@ -4,6 +4,7 @@ const collapseOverlapsGeneratedFromRangeComparisonIfPossible = require('./collap
 const zeroSubrangeByContainerRange = require('./zeroSubrangeByContainerRange')
 const normalizePositionByRangeLength = require('./normalizePositionByRangeLength')
 
+//gets the overlapping sections of two ranges and zeroes them to their first point of intersection!
 module.exports = function getZeroedRangeOverlaps (annotation, selection, sequenceLength)  {
   const overlaps = collapseOverlapsGeneratedFromRangeComparisonIfPossible(getOverlapsOfPotentiallyCircularRanges(annotation, selection, sequenceLength), sequenceLength, annotation)
   const zeroedOverlaps = overlaps.map((overlap) => {
