9b89c77bec514729638eee6b512a819d9a3bb351
Thomas Rich <tnrich@teselagen.com>
2021-09-02 10:28:50 -0700

    adding a new overlapsSelf option to getRangeLength

--- src/getRangeLength.js
+++ src/getRangeLength.js
@@ -1,9 +1,14 @@
-var provideInclusiveOptions = require('./provideInclusiveOptions')
-module.exports = provideInclusiveOptions(getRangeLength)
+var provideInclusiveOptions = require("./provideInclusiveOptions");
+module.exports = provideInclusiveOptions(getRangeLength);
 function getRangeLength(range, rangeMax) {
-    if (range.end < range.start) {
-       return rangeMax - range.start + range.end + 1;
-    } else {
-       return range.end - range.start + 1;
-    }
+  let toRet;
+  if (range.end < range.start) {
+    toRet = rangeMax - range.start + range.end + 1;
+  } else {
+    toRet = range.end - range.start + 1;
+  }
+  if (range.overlapsSelf && rangeMax) {
+    toRet += rangeMax;
+  }
+  return toRet;
 }
--- src/getRangeLength.test.js
+++ src/getRangeLength.test.js
@@ -7,18 +7,27 @@
 // 3 1
 //  2
 
-var assert = require('assert');
-var getRangeLength = require('./getRangeLength');
-describe('getRangeLength', function () {
-	it('should return the correct length for ranges that cross the origin', function (done) {
-		var length = getRangeLength({start: 9,end:0},10)
-		assert(length === 2)
-		done()
-	});
-	it('should return the correct length for ranges that do not cross the origin', function (done) {
-		var length = getRangeLength({start: 4,end:6},10)
-		assert(length === 3)
-		done()
-	});
+const assert = require("assert");
+const getRangeLength = require("./getRangeLength");
+describe("getRangeLength", function() {
+  it("should return the correct length for ranges that cross the origin", function(done) {
+    const length = getRangeLength({ start: 9, end: 0 }, 10);
+    assert(length === 2);
+    done();
+  });
+  it("should return the correct length for ranges that do not cross the origin", function(done) {
+    const length = getRangeLength({ start: 4, end: 6 }, 10);
+    assert(length === 3);
+    done();
+  });
+  it("should return the correct length for ranges that overlapSelf", function(done) {
+    const length = getRangeLength({ start: 4, end: 6, overlapsSelf: true }, 10);
+    assert(length === 13);
+    done();
+  });
+  it("should return the correct length for ranges that overlapSelf and origin", function(done) {
+    const length = getRangeLength({ start: 9, end: 1, overlapsSelf: true }, 10);
+    assert(length === 13);
+    done();
+  });
 });
-
